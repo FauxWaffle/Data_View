881c13e8c77b1f896698a00810016d018ec4c382
Orrin Dabney <127357534+FauxWaffle@users.noreply.github.com>
2024-03-12 15:41:54 -0500

    Update main.py
    Added tabs for data controls. Secondary tab shows SID accounts as file owner.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import stream_back
 import pandas as pd
-
+from stream_back import *
 
 st.set_page_config(layout="wide")
 metrics = stream_back.df_all()
 
 st.header("Data Summary:")
+
+
 col_data_1, col_data_2, col_data_3 = st.columns(3)
 
 #--Computing Data Metrics--#
@@ -37,20 +39,33 @@ st.markdown("---")
 
 
 #--Toggles for Data Controls--#
-
 st.header('Data Controls:')
+tab_main, tab_second = st.tabs(["All Data", "No File Owner"])
+
 
 col1, col2 = st.columns(2)
 
+with tab_main:
+    with col1:
+        metric_1 = st.toggle("File Size - MB")
+        metric_2 = st.toggle("Created On")
+        metric_3 = st.toggle("Last Access")
 
-with col1:
-    metric_1 = st.toggle("File Size - MB")
-    metric_2 = st.toggle("Created On")
-    metric_3 = st.toggle("Last Access")
-with col2:
-    metric_4 = st.toggle("Last Modify")
-    metric_5 = st.toggle("File Owner")
-    metric_6 = st.toggle("Never Accessed - Click Header to Filter YES")
+    with col2:
+        metric_4 = st.toggle("Last Modify")
+        metric_5 = st.toggle("File Owner")
+        metric_6 = st.toggle("Never Accessed - Click Header to Filter YES")
+
+sid_count = metrics['SID as Owner'].count()
+with tab_second:
+    st.write("Total Number of Unowned Files:", sid_count)
+    export_data = convert_df(metrics['Export Results'])
+    st.download_button(
+        label="Download SID list to CSV",
+        data=export_data,
+        file_name="Files_no_owner.csv",
+        mime='text/csv'
+    )
 
 
 display_df = pd.DataFrame({'File Name': metrics['File Name']})
@@ -58,7 +73,6 @@ display_df = pd.DataFrame({'File Name': metrics['File Name']})
 
 if metric_1:
     display_df['File Size'] = metrics['File Size']
-    
 if metric_2:
     display_df['Created On'] = metrics['Created On']
     
@@ -74,10 +88,25 @@ if metric_5:
 if metric_6:
     display_df['Never Accessed'] = metrics['Not Touched']
 
+
+
 st.dataframe(display_df, use_container_width=False)
 
 
+#--SID as Owner--#
+
+
 st.markdown("---")
+with tab_second:
+    deleted_owner = {
+        'File Name': metrics['SID File'],
+        'No File Owner': metrics['SID as Owner']
+    }
+
+    sid_df = pd.DataFrame(deleted_owner)
+
+    st.table(sid_df)
+
 
 
 #--Dataframe build for Table--#
